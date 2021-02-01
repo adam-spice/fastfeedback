7f97cb4482d3d697d83cb582fde9bf667a5dd3d9
Adam Spice <adam@adam-spice.online>
2021-02-01 13:50:58 +1300

    renamed auth provider, added chakraui, setup theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,42 @@
-import { ProvideAuth } from '../lib/auth';
-import '../styles/globals.css';
+import { AuthProvider } from '@/lib/auth';
+import theme from '@/styles/theme';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
+const GlobalStyle = ({ children }) => {
   return (
-    <ProvideAuth>
-      <Component {...pageProps} />
-    </ProvideAuth>
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
+function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
-export default MyApp;
+export default App;
